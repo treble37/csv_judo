7447e9e7097eca39fe569710da467ed0239df3b5
BruceP <brucepark0@gmail.com>
2013-08-02 14:04:26 -0700

    removed self calls from CSVJudo Module

--- lib/csv_judo.rb
+++ lib/csv_judo.rb
@@ -1,14 +1,14 @@
 require 'csv'
 
 module CSVJudo
-  def self.read_in_csv(file_name)
+  def read_in_csv(file_name)
     csv_arr = Array.new
     CSV.foreach(file_name) do |row|
       csv_arr<<row
     end
     csv_arr
   end
-  def self.fetch_rows_by_date(csv_arr,arg_options={})
+  def fetch_rows_by_date(csv_arr,arg_options={})
     options = {start_date: "2013-07-26 22:51", end_date: "2013-07-27 22:51", column: 4}
     options.merge!(arg_options)
     start_dt=DateTime.parse(options[:start_date])
@@ -22,8 +22,8 @@ module CSVJudo
     end
     return match_arr
   end
-  def self.fetch_rows_by_duration(csv_arr,arg_options={})
-    #duration_search: 1: >= start_duration, 0: >=start_duration & <=end_duration, -1: <end_duration
+  def fetch_rows_by_duration(csv_arr,arg_options={})
+    #duration_search: 1: >= start_duration, 0: >=start_duration & <=end_duration, -1: <=end_duration
     options = {start_duration: DateTime.strptime("00:00:30","%H:%M:%S"), end_duration: DateTime.strptime("00:01:00","%H:%M:%S"), column: 9, duration_search: 1}
     options.merge!(arg_options)
     match_arr = Array.new
--- main.rb
+++ main.rb
@@ -1,10 +1,5 @@
 require_relative 'lib/csv_judo'
 
-csv_arr = CSVJudo.read_in_csv("sample_usage_report.csv")
-csv_arr = csv_arr[1..csv_arr.length-1]
-#csv_arr = CSVJudo.fetch_rows_by_date(csv_arr,{start_date: "2013-7-23", end_date: "2013-7-25"})
-csv_arr = CSVJudo.fetch_rows_by_duration(csv_arr,options = {start_duration: DateTime.strptime("00:03:00","%H:%M:%S"), end_duration: DateTime.strptime("00:01:00","%H:%M:%S"), column: 9, duration_search: 1})
-
 class ReportGenerator
   include CSVJudo
   attr_accessor :file_name_arr, :num_calls_arr, :report_fields_arr, :date_arr, :duration_arr, :final_report
@@ -17,19 +12,19 @@ class ReportGenerator
   def report_generator
     @final_report<<@report_fields_arr
     @file_name_arr.each do |f|
-      report_csv_arr = CSVJudo.read_in_csv(f)
+      report_csv_arr = read_in_csv(f)
       report_csv_arr = report_csv_arr[1..report_csv_arr.length-1]
-      report_csv_arr = CSVJudo.fetch_rows_by_date(report_csv_arr,{start_date: @date_arr[0], end_date: @date_arr[1]})
-      report_csv_arr = CSVJudo.fetch_rows_by_duration(report_csv_arr,options = {start_duration: @duration_arr[0], end_duration: @duration_arr[1], column: 9, duration_search: @duration_arr[2]})
-      @final_report<<["",report_csv_arr.length]
+      report_csv_arr = fetch_rows_by_date(report_csv_arr,{start_date: @date_arr[0], end_date: @date_arr[1]})
+      report_csv_arr = fetch_rows_by_duration(report_csv_arr,options = {start_duration: @duration_arr[0], end_duration: @duration_arr[1], column: 9, duration_search: @duration_arr[2]})
+      @final_report<<[f,report_csv_arr.length]
     end
   end
 end
 
 repg = ReportGenerator.new
-repg.date_arr = ["2013-7-23","2013-7-27"]
-repg.file_name_arr = Dir['*.csv']
-repg.duration_arr = [DateTime.strptime("00:00:00","%H:%M:%S"),DateTime.strptime("00:01:00","%H:%M:%S"),0]
+repg.date_arr = ["2013-7-29","2013-7-30"]
+repg.file_name_arr = Dir['*cloudsharklabs*.csv']
+repg.duration_arr = [DateTime.strptime("00:00:00","%H:%M:%S"),DateTime.strptime("00:03:00","%H:%M:%S"),0]
 repg.report_generator
 
 p repg.final_report
